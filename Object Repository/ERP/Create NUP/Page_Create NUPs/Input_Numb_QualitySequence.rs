101bb45c77d8536fa27df7983c38c2039d7e05b2
mulkilutfhi <56911844+mulkilutfhi@users.noreply.github.com>
2020-02-13 03:02:31 +0700

    add create NUP

---
+++ Object Repository/ERP/Create NUP/Page_Create NUPs/Input_Numb_QualitySequence.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input_Numb_QualitySequence</name>
+   <tag></tag>
+   <elementGuidId>f072c30e-e63b-45d2-b5d6-8ef0eddcc16c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;slotform-numberTags&quot;]/div/div[1]/input</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
