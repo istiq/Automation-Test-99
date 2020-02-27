a2546074eea17a6ba18ad19ea772232cbbb0f73b
indrikuryawati <indrikuryawati@10.10.10.239>
2020-02-27 10:37:07 +0700

    run test

---
+++ Object Repository/ERP/Create Project/Page_Add Project/Test_Url.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Test_Url</name>
+   <tag></tag>
+   <elementGuidId>019ebff1-a871-41da-8345-1e74526e76af</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;projectform-videourl&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
